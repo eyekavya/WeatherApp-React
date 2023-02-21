d6fb59c26fb0c09dd2fc029c360b9bb9ea197586
eyekavya <kavyasrivastava7830@gmail.com>
2023-02-21 22:41:45 +0530

    added search input and styled component

---
+++ src/components/SearchInput/StyledSearchInput.js
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+
+export const StyledSearchInput = styled.section`
+  -webkit-font-smoothing: antialZiased;
+  color: #acbdce;
+  display: flex;
+  justify-content: center;
+  margin: 6rem;
+
+  body,
+  html {
+    background: #e2e9f4;
+    display: grid;
+    height: 100%;
+    grid-template: 1fr/100%;
+    place-items: center;
+  }
+
+  .Card {
+    padding: 1px;
+    border-radius: 10px;
+    background: linear-gradient(
+      -67deg,
+      rgba(#c8d8e7, 0.7),
+      rgba(255, 255, 255, 0.8)
+    );
+    overflow: hidden;
+    box-shadow: -2px -2px 6px rgba(#fff, 0.6), 2px 2px 12px #c8d8e7;
+    width: 380px;
+  }
+
+  .CardInner {
+    padding: 16px 16px;
+    background-color: #e2e9f4;
+    border-radius: 10px;
+  }
+
+  .container {
+    display: flex;
+  }
+
+  .Icon {
+    min-width: 46px;
+    min-height: 46px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+    margin-right: 12px;
+    box-shadow: -2px -2px 6px rgba(#fff, 0.6), 2px 2px 12px #c8d8e7;
+
+    svg {
+      transform: translate(-1px, -1px);
+    }
+  }
+
+  label {
+    font-family: "Hind", sans-serif;
+    display: block;
+    color: #3c4b66;
+    margin-bottom: 12px;
+    background: linear-gradient(45deg, rgba(#6b7b8f, 1), #3c4b66);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  .InputContainer {
+    width: 100%;
+  }
+
+  input {
+    background-color: #e3edf7;
+    padding: 16px 32px;
+    border: none;
+    display: block;
+    font-family: "Orbitron", sans-serif;
+    font-weight: 600;
+    color: #a9b8c9;
+    -webkit-appearance: none;
+    transition: all 240ms ease-out;
+    width: 100%;
+
+    &::placeholder {
+      color: #6d7f8f;
+    }
+
+    &:focus {
+      outline: none;
+      color: #6d7f8f;
+      background-color: lighten(#e3edf7, 3%);
+    }
+  }
+
+  .InputContainer {
+    position: relative;
+    border-radius: 10px;
+    overflow: hidden;
+
+    &:before,
+    &:after {
+      left: 0;
+      top: 0;
+      display: block;
+      content: "";
+      pointer-events: none;
+      width: 100%;
+      height: 100%;
+      position: absolute;
+    }
+
+    &:before {
+      box-shadow: inset -2px -2px 4px rgba(255, 255, 255, 0.7);
+    }
+
+    &:after {
+      box-shadow: inset 1px 1px 3px #c5d4e3, inset 2px 2px 6px #c5d4e3;
+    }
+  }
+`;
