8e262a8826d58cb3f624de72488a360f41a015cc
eyekavya <kavyasrivastava7830@gmail.com>
2023-02-22 21:57:25 +0530

    integrated with api

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { fetchWeather } from "../../weather-data/fetchWeather";
 import { StyledCard } from "./StyledCard";
 
 function Card() {
   return (
     <StyledCard>
-      <div class="container">
-        <div class="card"></div>
+      <div className="container">
+        <div className="card"></div>
 
-        <div class="drops">
-          <div class="drop drop-1"></div>
-          <div class="drop drop-2"></div>
-          <div class="drop drop-3"></div>
-          <div class="drop drop-4"></div>
-          <div class="drop drop-5"></div>
+        <div className="drops">
+          <div className="drop drop-1"></div>
+          <div className="drop drop-2"></div>
+          <div className="drop drop-3"></div>
+          <div className="drop drop-4"></div>
+          <div className="drop drop-5"></div>
         </div>
       </div>
     </StyledCard>
